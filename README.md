fe7f5ee69f0c08d103c45611cd273207e9a84eda
DiblikMendizabal <dantdiblk@hotmail.com>
2025-04-10 11:35:01 -0600

    Corrección me gusta y formularios

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# Titulo:
-# Descripcion:  Está es una una página interactiva en la que los usuarios pueden añadir, eliminar o dar "me gusta" a las fotos.
-# Tecnologias: JavaScript, @font-face, Figma, Media Queries, GitHub, CSS Grid Layout, Sombreados y degradados en CSS para mejorar el diseño visual, Arrays, Funciones dar like a las tarjetas y Manipulación del DOM.
-# Autor: Diblik Mendizábal
+# Tripleten web_project_homeland
+
+Proyecto 8 - Logica y metodos de programacion en JavaScript
+
+El objetivo de este proyecto es implementar y optimizar la lógica de programación en JavaScript para gestionar dinámicamente tarjetas de contenido, permitiendo su creación, eliminación y la interacción con botones de like y dislike.
+
+En este proyecto, se adquirieron conocimientos sobre el manejo de Arrays para almacenar tarjetas, Funciones para modularizar el código y Manipulación del DOM para actualizar la interfaz de usuario en tiempo real.
+
+## Tecnologías utilizadas
+
+- **JavaScript** para la manipulación del DOM.
+- **@font-face** para agregar y gestionar fuentes personalizadas.
+- **Figma** para definir la tipografía y dimensiones del diseño.
+- **Media Queries** para implementar un diseño responsivo adaptable a diferentes dispositivos.
+- **GitHub** como plataforma para el control de versiones y alojamiento del código.
+- **CSS Grid Layout** para estructurar la interfaz.
+- **Sombreados y degradados en CSS** para mejorar el diseño visual.
+- **Arrays** para almacenar y gestionar las tarjetas de contenido.
+- **Funciones** para agregar, eliminar y dar like o dislike a las tarjetas.
+- **Manipulación del DOM** para actualizar la lista de tarjetas en tiempo real.
+
+## Autor
+
+**Diblik**
+
+https://diblikmedizabal.github.io/web_project_around/
--- scripts/index.js
+++ scripts/index.js
@@ -1,142 +1,122 @@
 const openPopup = document.querySelector(".popup");
- const buttonPopup = document.querySelector(".profile__info-button");
- const buttonClosePopup = document.querySelector(".popup__button-close");
- const formElement = document.querySelector(".popup__form");
- const nameInput = document.querySelector(".popup__form-name");
- const jobInput = document.querySelector(".popup__form-occupation");
- const displayName = document.querySelector(".profile__details-name");
- const displayDescription = document.querySelector(".profile__details-description");
- const buttonSubmit = document.querySelector(".popup__form-button");
+const buttonPopup = document.querySelector(".profile__info-button");
+const buttonClosePopup = document.querySelector(".popup__button-close");
+const formElement = document.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__form-name");
+const jobInput = document.querySelector(".popup__form-occupation");
+const displayName = document.querySelector(".profile__details-name");
+const displayDescription = document.querySelector(
+  ".profile__details-description"
+);
+const buttonSubmit = document.querySelector(".popup__form-button");
 
- function editProfile() {
-   handlePopupOpen();
-   nameInput.value = displayName.textContent;
-   jobInput.value = displayDescription.textContent;
- }
- const openPopupAdd = document.querySelector(".popup-add");
- const buttonAdd = document.querySelector(".profile__info-button-add");
- const buttonCloseAdd = document.querySelector(".popup-add__button-close");
+const openPopupAdd = document.querySelector(".popup-add");
+const buttonAdd = document.querySelector(".profile__info-button-add");
+const buttonCloseAdd = document.querySelector(".popup-add__button-close");
 
- const gallery = document.querySelector(".gallery");
- const formAdd = document.querySelector(".popup-add__form");
- const descriptionInput = document.querySelector(".popup-add__form-name");
- const imageInput = document.querySelector(".popup-add__form-occupation");
- const openPopupImage = document.querySelector(".popup__image");
- const imageButtonClose = document.querySelector(".popup__image-button-close");
+const gallery = document.querySelector(".gallery");
+const formAdd = document.querySelector(".popup-add__form");
+const descriptionInput = document.querySelector(".popup-add__form-name");
+const imageInput = document.querySelector(".popup-add__form-occupation");
+const openPopupImage = document.querySelector(".popup__image");
+const imageButtonClose = document.querySelector(".popup__image-button-close");
 
- const initialCards = [
-   {
-     name: "Valle de Yosemite",
-     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-   },
-   {
-     name: "Lago Louise",
-     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-   },
-   {
-     name: "Montañas Calvas",
-     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-   },
-   {
-     name: "Latemar",
-     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-   },
-   {
-     name: "Parque Nacional de la Vanoise",
-     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-   },
-   {
-     name: "Lago di Braies",
-     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-   },
- ];
- //
- function handlePopupOpen() {
-   openPopup.classList.add("popup_opened");
- }
- @@ -24,15 +56,87 @@{
-   openPopup.classList.remove("popup_opened");}
+const initialCards = [
+  {name: "Valle de Yosemite", link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",},
+  {name: "Lago Louise", link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",},
+  {name: "Montañas Calvas", link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",},
+  {name: "Latemar", link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",},
+  {name: "Parque Nacional de la Vanoise", link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",},
+  {name: "Lago di Braies", link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",},
+];
 
- function editProfile() {
-   handlePopupOpen();
-   nameInputvalue = displayName.textContent;
-   jobInput.value = displayDescription.textContent;
- }
+function handlePopupOpen() {
+  openPopup.classList.add("popup_opened");
+}
 
- function handleProfileFormSubmit(evt) {
-   evt.preventDefault();
-   displayName.textContent = nameInput.value;
-   displayDescription.textContent = jobInput.value;
-   openPopup.classList.remove("popup_opened");
- }
- //
- function handlePopupAddOpen() {
-   openPopupAdd.classList.add("popup-add_opened");
- }
+function handlePopupClose() {
+  openPopup.classList.remove("popup_opened");
+}
 
- formElement.addEventListener("submit", handleProfileFormSubmit);
- function handlePopupAddClose() {
-   openPopupAdd.classList.remove("popup-add_opened");
- }
+function editProfile() {
+  handlePopupOpen();
+  nameInput.value = displayName.textContent;
+  jobInput.value = displayDescription.textContent;
+}
 
- function addCards() {
-   initialCards.forEach((item) => {
-     const card = createCard(item.name, item.link);
-     gallery.append(card);
-     openPopupAdd.classList.remove("popup-add_opened");
-   });
- }
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  displayName.textContent = nameInput.value;
+  displayDescription.textContent = jobInput.value;
+  openPopup.classList.remove("popup_opened");
+}
 
- function createCard(name, link) {
-   const templateGallery = document.querySelector("#template").content;
-   const card = templateGallery.querySelector(".gallery__card").cloneNode(true);
-   const cardImage = card.querySelector(".gallery__card-image");
-   const cardText = card.querySelector(".gallery__card-name");
-   const deleteButton = card.querySelector(".gallery__card-delete");
-   const like = card.querySelector(".gallery__card-like");
+function handlePopupAddOpen() {
+  openPopupAdd.classList.add("popup-add_opened");
+}
 
-   cardImage.src = link;
-   cardImage.alt = name;
-   cardText.textContent = name;
+function handlePopupAddClose() {
+  openPopupAdd.classList.remove("popup-add_opened");
+}
 
-   deleteButton.addEventListener("click", () => {
-     card.remove();
-   });
-   like.addEventListener("click", () => {
-     like.classList.toggle("gallery__card-like-active");
-   });
-   cardImage.addEventListener("click", () => {
-     handlePopupImageOpen(name, link);
-   });
-   return card;
- }
+function addCards() {
+  initialCards.forEach((item) => {
+    const card = createCard(item.name, item.link);
+    gallery.append(card);
+    openPopupAdd.classList.remove("popup-add_opened");
+  });
+}
 
- function handleImageFormSubmit(evt) {
-   evt.preventDefault();
-   const card = createCard(descriptionInput.value, imageInput.value);
-   gallery.prepend(card);
-   openPopupAdd.classList.remove("popup-add_opened");
- }
+function createCard(name, link) {
+  const templateGallery = document.querySelector("#template").content;
+  const card = templateGallery.querySelector(".gallery__card").cloneNode(true);
+  const cardImage = card.querySelector(".gallery__card-image");
+  const cardText = card.querySelector(".gallery__card-name");
+  const deleteButton = card.querySelector(".gallery__card-delete");
+  const like = card.querySelector(".gallery__card-like");
 
- function handlePopupImageOpen(name, link) {
-   const popupImg = openPopupImage.querySelector(".popup__img");
-   const popupText = openPopupImage.querySelector(".popup__text");
-   popupImg.src = link;
-   popupImg.alt = name;
-   popupText.textContent = name;
-   openPopupImage.classList.add("popup__image_opened");
- }
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardText.textContent = name;
 
- function handlePopupImageClose() {
-   openPopupImage.classList.remove("popup__image_opened");
- }
+  deleteButton.addEventListener("click", () => {
+    card.remove();
+  });
+  like.addEventListener("click", () => {
+    like.classList.toggle("gallery__card-like-active");
+  });
+  cardImage.addEventListener("click", () => {
+    handlePopupImageOpen(name, link);
+  });
+  return card;
+}
 
- buttonPopup.addEventListener("click", handlePopupOpen);
- buttonPopup.addEventListener("click", editProfile);
- buttonClosePopup.addEventListener("click", handlePopupClose);
- formElement.addEventListener("submit", handleProfileFormSubmit);
- buttonAdd.addEventListener("click", handlePopupAddOpen);
- buttonCloseAdd.addEventListener("click", handlePopupAddClose);
- addCards();
- formAdd.addEventListener("submit", handleImageFormSubmit);
- imageButtonClose.addEventListener("click", handlePopupImageClose);
+function handleImageFormSubmit(evt) {
+  evt.preventDefault();
+  const card = createCard(descriptionInput.value, imageInput.value);
+  gallery.prepend(card);
+  openPopupAdd.classList.remove("popup-add_opened");
+}
+
+function handlePopupImageOpen(name, link) {
+  const popupImg = openPopupImage.querySelector(".popup_img");
+  const popupText = openPopupImage.querySelector(".popup__text");
+  popupImg.src = link;
+  popupImg.alt = name;
+  popupText.textContent = name;
+  openPopupImage.classList.add("popup__image_opened");
+}
+
+function handlePopupImageClose() {
+  openPopupImage.classList.remove("popup__image_opened");
+}
+
+buttonPopup.addEventListener("click", handlePopupOpen);
+buttonPopup.addEventListener("click", editProfile);
+buttonClosePopup.addEventListener("click", handlePopupClose);
+formElement.addEventListener("submit", handleProfileFormSubmit);
+buttonAdd.addEventListener("click", handlePopupAddOpen);
+buttonCloseAdd.addEventListener("click", handlePopupAddClose);
+addCards();
+formAdd.addEventListener("submit", handleImageFormSubmit);
+imageButtonClose.addEventListener("click", handlePopupImageClose);
